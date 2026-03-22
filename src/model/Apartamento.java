5386c3b7a3ff34bf6c590c9d86480576ccda5463
Rafael Gualberto <rafaelgualberto58@gmail.com>
2026-03-22 01:39:04 -0300

    andar, apartamento e edificio

--- src/model/Apartamento.java
+++ src/model/Apartamento.java
@@ -19,4 +19,72 @@ public class Apartamento {
     }
 
     //TODO: Criar os getters e setter
+
+
+    public Apartamento(StatusApartamento status, int numero, int andar, double metragem, int quantidadeDeQuartos, int quantidadeDeBanheiros, double valorDeVenda, double valorSinal) {
+        this.status = status;
+        this.numero = numero;
+        this.andar = andar;
+        this.metragem = metragem;
+        this.quantidadeDeQuartos = quantidadeDeQuartos;
+        this.quantidadeDeBanheiros = quantidadeDeBanheiros;
+        this.valorDeVenda = valorDeVenda;
+        this.valorSinal = valorSinal;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public double getMetragem() {
+        return metragem;
+    }
+
+    public void setMetragem(double metragem) {
+        this.metragem = metragem;
+    }
+
+    public int getQuantidadeDeQuartos() {
+        return quantidadeDeQuartos;
+    }
+
+    public void setQuantidadeDeQuartos(int quantidadeDeQuartos) {
+        this.quantidadeDeQuartos = quantidadeDeQuartos;
+    }
+
+    public int getQuantidadeDeBanheiros() {
+        return quantidadeDeBanheiros;
+    }
+
+    public void setQuantidadeDeBanheiros(int quantidadeDeBanheiros) {
+        this.quantidadeDeBanheiros = quantidadeDeBanheiros;
+    }
+
+    public double getValorDeVenda() {
+        return valorDeVenda;
+    }
+
+    public void setValorDeVenda(double valorDeVenda) {
+        this.valorDeVenda = valorDeVenda;
+    }
+
+    public double getValorSinal() {
+        return valorSinal;
+    }
+
+    public void setValorSinal(double valorSinal) {
+        this.valorSinal = valorSinal;
+    }
 }
--- src/model/Edificio.java
+++ src/model/Edificio.java
@@ -9,4 +9,45 @@ public class Edificio {
     private ArrayList<Andar> andares;
 
     //TODO: Criar getters, setters
+
+    public Edificio(int id, String nome, String endereco, ArrayList<Andar> andares) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.andares = andares;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public ArrayList<Andar> getAndares() {
+        return andares;
+    }
+
+    public void setAndares(ArrayList<Andar> andares) {
+        this.andares = andares;
+    }
 }
+
+
